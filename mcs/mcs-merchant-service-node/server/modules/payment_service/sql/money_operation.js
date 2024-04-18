9f15fdf3cffd49de14010e4d8c59197e72e44ca5
nabindhami14 <nabindhami4747@gmail.com>
2024-04-18 17:05:58 +0545

    fix:customer: payment parsing

--- mcs/mcs-merchant-service-node/server/modules/payment_service/sql/money_operation.js
+++ mcs/mcs-merchant-service-node/server/modules/payment_service/sql/money_operation.js
@@ -4,20 +4,20 @@ const { mysqlHelper, keyHelper } = require('common/helpers');
 
 module.exports = async (id, amount, operation) => {
     try {
-        let response = { data: {} };
+        const response = { data: {} };
         
         const query = mysqlHelper.format(`SELECT id, name, email, address, phone_number, money FROM customers WHERE id = ?`, id);
-        let [dbResponse] = await mysqlHelper.query(query);
+        const [dbResponse] = await mysqlHelper.query(query);
 
         if (dbResponse && dbResponse.length > 0) {
             const customer = keyHelper.keysToCamel(dbResponse[0]);
 
             let updatedMoney;
             if (operation === 'add') {
-                updatedMoney = +(customer.money) + +amount;
-            }else {
-                updatedMoney = +(customer.money) - +amount;
-            } 
+                updatedMoney = parseFloat(customer.money) + parseFloat(amount);
+            } else {
+                updatedMoney = parseFloat(customer.money) - parseFloat(amount);
+            }
 
             const updateQuery = mysqlHelper.format(`UPDATE customers SET money = ? WHERE id = ?`, [updatedMoney, id]);
             await mysqlHelper.query(updateQuery);
