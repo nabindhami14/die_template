01e3c240a651dbe69a0a9d00c1402c598c6ed41a
nabindhami14 <nabindhami4747@gmail.com>
2024-04-21 11:57:48 +0545

    feat: refactoring

--- api-gateway/api-admin/server/routes/customer-service/route.js
+++ api-gateway/api-admin/server/routes/customer-service/route.js
@@ -6,17 +6,12 @@
     const customerRouter = express.Router();
     const customerRouterCtrl = require(".")
 
-    const { authMiddleware, fileUploadMiddleware } = require("common/helpers")
+    const { fileUploadMiddleware } = require("common/helpers")
 
 
     customerRouter.route("/").post(customerRouterCtrl.CreateCustomer)
     customerRouter.route("/bulk-upload").post(fileUploadMiddleware, customerRouterCtrl.BulkUpload)
 
-
-
-    // customerRouter.route("/:customerId").get(authMiddleware, customerRouterCtrl.GetCustomer)
-    // customerRouter.route("/").get(authMiddleware, customerRouterCtrl.GetCustomers)
-    
     customerRouter.route("/").get(customerRouterCtrl.GetCustomers)
     customerRouter.route("/:customerId").get(customerRouterCtrl.GetCustomer)
 
--- api-gateway/api-admin/server/routes/index.js
+++ api-gateway/api-admin/server/routes/index.js
@@ -4,13 +4,15 @@ const customerRouter = require("./customer-service/route");
 const merchantRouter = require("./merchant-service/route");
 const authRouter = require("./auth-service/route");
 
+const { authMiddleware } = require("common/helpers")
+
 const express = require("express");
 const router = express.Router();
 
 (() => {
-    router.use("/customers", customerRouter);
-    router.use("/merchants", merchantRouter);
     router.use("/auth", authRouter);
+    router.use("/customers", authMiddleware, customerRouter);
+    router.use("/merchants", authMiddleware, merchantRouter);
 
     module.exports = router;
 })();
