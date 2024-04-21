739cf8a9aeb348a2c2eea3b9a9aec226bbae5d4e
nabindhami14 <nabindhami4747@gmail.com>
2024-04-21 13:52:08 +0545

    feat:merchant: create merchant atomic

--- mcs/mcs-merchant-service-node/server/modules/merchant_service/methods/create_merchant.js
+++ mcs/mcs-merchant-service-node/server/modules/merchant_service/methods/create_merchant.js
@@ -1,9 +1,11 @@
 (() => {
-    const sql = require("../sql")
+    const sql = require("../sql");
+    const { mysqlHelper } = require('common/helpers');
+
     module.exports = async (call, callback) => {
         const { name, auth_type } = call.request;
 
-        const merchant = await sql.getMerchantByName(name)
+        const merchant = await sql.getMerchantByName(name);
         if (merchant.data.id) {
             return callback(null, { status: 400, success: false, message: "Merchant already exists with this name" });
         }
@@ -12,33 +14,24 @@
             0: "BASIC",
             1: "OAUTH2",
             2: "JWT"
-        }
+        };
 
         try {
-            if (auth_type === 0) {
-                const authPayload = `username VARCHAR(255), password VARCHAR(255)`
+            const authPayloads = {
+                0: "username VARCHAR(255), password VARCHAR(255)",
+                1: "access_token VARCHAR(255)",
+                2: "token VARCHAR(255)"
+            };
 
-                await sql.createMerchantTable(name, authPayload)
-                await sql.createMerchant(name, authPayloadMap[0])
-
-                return callback(null, { status: 200, success: true });
-            } else if (auth_type === 1) {
-                const authPayload = `access_token VARCHAR(255)`
-
-                await sql.createMerchantTable(name, authPayload)
-                await sql.createMerchant(name, authPayloadMap[1])
-                return callback(null, { status: 200, success: true });
-            } else {
-                const authPayload = `token VARCHAR(255)`
-
-                await sql.createMerchantTable(name, authPayload)
-                await sql.createMerchant(name, authPayloadMap[2])
-                return callback(null, { status: 200, success: true });
-            }
+            await mysqlHelper.beginTransaction();
+            const authPayload = authPayloads[auth_type];
+            await sql.createMerchantTable(name, authPayload);
+            await sql.createMerchant(name, authPayloadMap[auth_type]);
+            await mysqlHelper.commit();
+            return callback(null, { status: 200, success: true });
         } catch (error) {
+            await mysqlHelper.rollback(); // Rollback the transaction
             callback(error);
         }
     };
-
-})()
-
+})();
